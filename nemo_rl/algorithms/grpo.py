b1a07e5291ffa2c819e066b90dffdb662aa7600c
Peter Jin <pjin@nvidia.com>
2026-04-16 17:29:25 -0700

    Set step_finished=True in async GRPO wandb logging

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -3833,7 +3833,8 @@ def async_grpo_train(
             logger.log_metrics(performance_metrics, step + 1, prefix="performance")
             logger.log_metrics(metrics, step + 1, prefix="train")
             logger.log_metrics(timing_metrics, step + 1, prefix="timing/train")
-            logger.log_metrics(efficiency_loggable, step + 1, prefix="")
+            # step_finished=True here since this is the final log of our current step.
+            logger.log_metrics(efficiency_loggable, step + 1, prefix="", step_finished=True)
 
             timer.reset()
             step += 1
