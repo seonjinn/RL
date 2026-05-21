54b42769fadc299080518a4dc4b309621f314e09
Seyed Taheri <smohsenitahe@nvidia.com>
2026-05-21 12:57:31 -0700

    Update the num of nodes in the jobs

--- batch_super_grpo.sh
+++ batch_super_grpo.sh
@@ -2,19 +2,19 @@ NEMORL=/lustre/fsw/portfolios/llmservice/users/smohsenitahe/NemoRL/super-v3-omni
 set -a
 source /lustre/fsw/portfolios/llmservice/users/smohsenitahe/.env
 set +a
-NUM_NODES=32
-JOB_NAME=grpo-super-v3-omni-vllm20-super-32n-cp2-10step-rawlp
+NUM_NODES=16
+JOB_NAME=grpo-super-v3-omni-vllm20-super-16n
 SEED=$(echo -n train:${JOB_NAME} | openssl dgst -md5 -binary | od -An -tu4 -N4 | xargs)
 MODEL_NAME="/lustre/fs1/portfolios/llmservice/users/ksapra/workspace/super_sft_16k/checkpoint"
 export CONTAINER=/lustre/fsw/portfolios/llmservice/users/smohsenitahe/sqsh/super-omni-vllm20-super-vlm2-20260507-0905b74.sqsh
-# export NRL_FORCE_REBUILD_VENVS=false
+
 export COMMAND="\
 uv run examples/run_vlm_grpo.py --config examples/configs/vlm_grpo_super_blend_random.yaml \
 cluster.num_nodes=$NUM_NODES \
 policy.model_name=${MODEL_NAME} \
 grpo.seed=$SEED \
 checkpointing.checkpoint_dir='results/${JOB_NAME}' \
-logger.wandb_enabled=false \
+logger.wandb_enabled=true \
 logger.wandb.name='${JOB_NAME}' \
 policy.tokenizer.chat_template=${MODEL_NAME}/chat_template.jinja \
 policy.max_total_sequence_length=8192 \
@@ -25,7 +25,8 @@ policy.megatron_cfg.moe_shared_expert_overlap=false \
 policy.generation.vllm_cfg.gpu_memory_utilization=0.55 \
 +policy.generation.vllm_cfg.logprobs_mode=raw_logprobs \
 +policy.generation.vllm_kwargs.disable_custom_all_reduce=true \
-grpo.max_num_steps=10 \
+grpo.max_num_steps=50 \
+checkpointing.checkpoint_must_save_by='00:03:45:00' \
 policy.megatron_cfg.scheduler.lr_warmup_iters=1 \
 +policy.megatron_cfg.scheduler.override_opt_param_scheduler=true \
 data.train.data_path='/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/datasets/eagle-next/image_data/rl_data/random_blend_v6.jsonl'"
