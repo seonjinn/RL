d102541f19a52477e0166a4b9a5c2a929450cb65
seonjinn <sna@nvidia.com>
2026-08-24 21:22:48 -0700

    exp(refit): run split-batch test on Ptyche
    Signed-off-by: seonjinn <sna@nvidia.com>

--- experiments/mxfp8_fused_refit_spike/submit_tests_ptyche.sh
+++ experiments/mxfp8_fused_refit_spike/submit_tests_ptyche.sh
@@ -36,6 +36,7 @@ test "\$(git -C ${REPO} rev-parse HEAD)" = "${EXPECTED_HEAD}"
 cd ${REPO}
 PYTHONPATH=${REPO} /opt/nemo_rl_venv/bin/python -m pytest -q \
   tests/unit/models/generation/test_mxfp8_prequant.py::test_batched_moe_shuffle_matches_per_expert \
+  tests/unit/models/generation/test_vllm_fp8_quantization.py::test_load_weights_accepts_prequantized_mxfp8_split_across_batches \
   tests/unit/models/generation/test_vllm_fp8_quantization.py::test_batched_moe_shuffle_matches_per_expert \
   tests/unit/models/generation/test_vllm_fp8_quantization.py::test_process_mxfp8_moe_refit_uses_batched_flashinfer_shuffle \
   tests/unit/models/generation/test_vllm_fp8_quantization.py::test_process_weights_after_loading_copies_in_place_on_refit \
